44878068461574c98f701a21e11f5a88212dba49
Szabó Ármin András <46499638+Lonelymous@users.noreply.github.com>
2022-08-30 03:32:25 +0200

    CreateEdit

---
+++ Controllers/ColourController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ColoursAPI.Models;
+using ColoursAPI.Data;
+
+namespace ColoursAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColourController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ColourController(ApiContext apiContext)
+        { 
+            _context = apiContext;
+        }
+
+        // Create/Edit
+        [HttpPost]
+        public JsonResult CreateEdit(Colour colour)
+        {
+            if (colour.Id == 0)
+            {
+                _context.Colours.Add(colour);
+            }
+            else
+            {
+                var colourInDb = _context.Colours.Find(colour.Id);
+
+                if (colourInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                colourInDb = colour;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(colour));
+        }
+    }
+}
---
+++ Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ColoursAPI.Models;
+
+namespace ColoursAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Colour> Colours { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        { 
+        
+        }
+    }
+}
